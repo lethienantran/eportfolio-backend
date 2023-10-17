193732789e5148ebc62048eb58c9b9806f8d123c
lethienantran <lethienantran@gmail.com>
2023-10-17 01:00:53 -0700

    Change README.md

--- src_code/src/configurations/database/DatabaseConfigurations.js
+++ src_code/src/configurations/database/DatabaseConfigurations.js
@@ -3,14 +3,14 @@ const knex = require("knex");
 
 /** Create cofig object for export development environment */
 const config = {
-    client: "mysql",
-    connection: {
-        host: "eportfolio-production.cwqsgyahoeq7.us-west-2.rds.amazonaws.com",
-        user: "eportfolio_db",
-        password:"UseMe2023.",
-        database:"eportfolio_prod",
-    },
-}
+  client: "mysql",
+  connection: {
+    host: "eportfolio-production.cwqsgyahoeq7.us-west-2.rds.amazonaws.com",
+    user: "eportfolio_db",
+    password: "",
+    database: "eportfolio_prod",
+  },
+};
 
 const db = knex(config);
 
